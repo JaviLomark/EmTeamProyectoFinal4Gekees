2af45156135fc10402ea64a7b367705ee32c3f75
Antoniomg3 <anjemuga2610@gmail.com>
2022-12-12 19:59:51 +0000

    Models recuperados

--- src/api/admin.py
+++ src/api/admin.py
@@ -2,6 +2,13 @@
 import os
 from flask_admin import Admin
 from .models import db, User
+from .models import db, Usuario
+from .models import db, Tipo_Empleo
+from .models import db, Provincia
+from .models import db, Candidato
+from .models import db, Sector
+from .models import db, Empresa
+from .models import db, Me_Gusta
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +19,13 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Usuario, db.session))
+    admin.add_view(ModelView(Tipo_Empleo, db.session))
+    admin.add_view(ModelView(Provincia, db.session))
+    admin.add_view(ModelView(Candidato, db.session))
+    admin.add_view(ModelView(Sector, db.session))
+    admin.add_view(ModelView(Empresa, db.session))
+    admin.add_view(ModelView(Me_Gusta, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
